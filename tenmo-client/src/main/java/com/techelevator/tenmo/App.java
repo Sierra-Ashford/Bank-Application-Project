49191cc27d062e975f2915a099960e481998c543
Sierra Ashford <sierraashford@gmail.com>
2023-11-29 18:45:44 -0500

    Capstone 2

--- tenmo-client/src/main/java/com/techelevator/tenmo/App.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/App.java
@@ -1,7 +1,9 @@
 package com.techelevator.tenmo;
 
+import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.AuthenticatedUser;
 import com.techelevator.tenmo.model.UserCredentials;
+import com.techelevator.tenmo.services.AccountService;
 import com.techelevator.tenmo.services.AuthenticationService;
 import com.techelevator.tenmo.services.ConsoleService;
 
@@ -11,6 +13,7 @@ public class App {
 
     private final ConsoleService consoleService = new ConsoleService();
     private final AuthenticationService authenticationService = new AuthenticationService(API_BASE_URL);
+    private AccountService accountService = new AccountService();
 
     private AuthenticatedUser currentUser;
 
@@ -86,6 +89,11 @@ public class App {
 
 	private void viewCurrentBalance() {
 		// TODO Auto-generated method stub
+        int userId = currentUser.getUser().getId();
+
+        Account account = accountService.getAccountByUserId(userId);
+        System.out.println("Your current balance is: $" + account.getBalance());
+
 		
 	}
 
---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
@@ -0,0 +1,31 @@
+package com.techelevator.tenmo.services;
+import com.techelevator.tenmo.model.Account;
+import com.techelevator.util.BasicLogger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+import com.techelevator.tenmo.model.AuthenticatedUser;
+import com.techelevator.tenmo.model.UserCredentials;
+public class AccountService {
+    public String API_BASE_URL = "http://localhost:8080/accounts/";
+    public String API_BASE_URL2 = "http://localhost:8080/accountByUserId/";
+    private final RestTemplate restTemplate = new RestTemplate();
+    public Account getAccountById(int accountId) {
+        Account account = null;
+        account = restTemplate.getForObject(API_BASE_URL + accountId, Account.class);
+        return account;
+    }
+
+    public Account getAccountByUserId(int userId) {
+        Account account = null;
+        account = restTemplate.getForObject(API_BASE_URL2 + userId, Account.class);
+        return account;
+    }
+
+}
